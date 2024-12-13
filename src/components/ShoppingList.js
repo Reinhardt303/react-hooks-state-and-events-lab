243d2dc6f064278d8364ab15ac522f5b0b342e06
Thomas Rains <thomasrains01@gmail.com>
2024-12-13 11:40:36 -0800

    submits hooks state and events lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,17 +1,27 @@
 import React, {useState} from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState(0)
-  function handleChange(event){
-    const newSelectedCategory = items.filter(item => item.category == selectedCategory)
-    setSelectedCategory(newSelectedCategory)
+function ShoppingList({ items }) { //I need the below to check if selected category matches item.category
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  // const xx = selectedCategory
+  const handleChange = (event) =>{displayGroceryList(event.target.value)}
+
+  const displayGroceryList = (value) => {  
+    /* console.log(value)
+    console.log(items)
+    console.log(newSelectedCategory) */
+    setSelectedCategory(value)
   }
+  function getFee(isMember) {
+    return isMember ? '$2.00' : '$10.00';
+  }
+  const filteredItemsArr = items.filter(item => selectedCategory === "All" ? true : item.category === selectedCategory) 
+
 
   return (
-    <div className="ShoppingList" onChange={handleChange}>
-      <div className="Filter">
-        <select name="filter">
+    <div className="ShoppingList" >
+      <div className="Filter" >
+        <select name="filter" onChange={handleChange}>  
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -19,7 +29,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItemsArr.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
